b9fe4ee67fa90b66f40df2970f79cd41f4b70d7f
Cihan <cihnyilmz@gmail.com>
2023-10-23 22:07:35 +0200

    added About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,8 +1,45 @@
 'use client';
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
 
 const About = () => (
-  <section>
-    About section
+  <section className={`${styles.paddings} relative z-10 `}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Cionomads" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] text-center text-[20px] font-normal text-secondary-white sm:text-[32px]"
+      >
+        The nomadic lifestyle is a gateway to{" "}
+        <span className="font-extrabold text-white">
+          limitless exploration.
+        </span>{" "}
+        It's a journey where you can experience the real world while staying
+        connected to your digital dreams. With the freedom of remote work and
+        the exhilaration of discovering new places,{" "}
+        <span className="font-extrabold text-white">
+          the nomad's world is your oyster.
+        </span>{" "}
+        Let's explore!
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="mt-[28px] h-[28px] w-[18px] object-contain"
+      />
+    </motion.div>
   </section>
 );
 
